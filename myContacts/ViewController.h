13067bedd3f04605bca824ad68bdfec7536a413a
DmitrJuga <dmitrjuga@gmail.com>
2015-05-25 22:39:19 +0300

    Initial Commit

---
+++ myContacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myContacts
+//
+//  Created by DmitrJuga on 25.05.15.
+//  Copyright (c) 2015 Dmitriy Dolotenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
